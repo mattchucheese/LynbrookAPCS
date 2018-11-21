6f219d9d327e0f2866aab98778b7eb6409c9904b
mattchucheese <44082644+mattchucheese@users.noreply.github.com>
2018-11-20 20:23:54 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+
+/**
+ * In class assignment
+ *
+ * @author: Matthew Lu
+ * @version: 8/22/18
+ * @author Period: 4
+ * @author Assignment: JMCh02_Hello - HelloWorld.java
+ *
+ * @author Sources: none
+ */
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, World!" );
+    }
+}
